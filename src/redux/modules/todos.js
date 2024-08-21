afb8f7e0d434b6399cfc48a9e55173bc51b0781e
Jo <johj703@nate.com>
2024-08-21 21:56:11 +0900

    Revert "react-redux, redux 설치"
    This reverts commit 1e6f404aec7aecdaf91c8ed283a22246a57d7d6f.

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,34 +1,3 @@
-// action value
-const ADD_TODO = "ADD_TODO";
-
-// action creator 만들어서 export(컴포넌트에서 사용할 수 있도록)
-export const addTodo = (payload) => {
-  return {
-    type: ADD_TODO,
-    payload: payload,
-  };
-};
-
-// state init
-const initialState = [
-  {
-    id: 1,
-    title: "react를 배워봅시다.",
-  },
-  {
-    id: 2,
-    title: "redux를 배워봅시다.",
-  },
-];
-
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case value:
-      break;
-
-    default:
-      break;
-  }
-};
+const todos = () => {};
 
 export default todos;
