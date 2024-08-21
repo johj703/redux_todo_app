d1b8a33c06efc6a8553981b2b5a4d123385a1f6b
Jo <johj703@nate.com>
2024-08-21 22:55:11 +0900

    Todo를 추가하는 로직을 추가 버튼에 넣기

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,7 +1,10 @@
 import { React, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/modules/todos";
 
 const AddForm = () => {
   const [todo, setTodo] = useState("");
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -12,7 +15,18 @@ const AddForm = () => {
           setTodo(e.target.value);
         }}
       />
-      <button>추가</button>
+      <button
+        onClick={() => {
+          dispatch(
+            addTodo({
+              id: 3,
+              title: todo,
+            })
+          );
+        }}
+      >
+        추가
+      </button>
     </div>
   );
 };
