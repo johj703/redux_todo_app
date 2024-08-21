60eaaa662f1af57d590e96c662715afe57fd306a
Jo <johj703@nate.com>
2024-08-21 22:34:40 +0900

    useSelector를 사용해서 스토어의 상태값을 반환하도록 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
 function App() {
+  const todos = useSelector((state) => state.todos);
+  console.log("todos", todos);
+
   return (
     <div>
       <h1>Redux TODO App</h1>
