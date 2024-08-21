aecc9b00d45094cf813cd9999c723e9b30db6f36
Jo <johj703@nate.com>
2024-08-21 22:29:42 +0900

    action value, action creator, state init 작성

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,7 +1,34 @@
 // action value
+const ADD_TODO = "ADD_TODO";
 
 // action creator 만들어서 export(컴포넌트에서 사용할 수 있도록)
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload: payload,
+  };
+};
 
-const todos = () => {};
+// state init
+const initialState = [
+  {
+    id: 1,
+    title: "react를 배워봅시다.",
+  },
+  {
+    id: 2,
+    title: "redux를 배워봅시다.",
+  },
+];
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
 
 export default todos;
