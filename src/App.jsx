c73f5160050e454b9834141a9c9b6f65bf702b69
Jo <johj703@nate.com>
2024-08-21 22:48:34 +0900

    저장된 id를 키로 지정하고 title을 뿌려줄 함수를 map을 사용해서 작성하고 app 파일에 return 값으로 넣기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import AddForm from "./components/AddForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <h1>Redux TODO App</h1>
       <AddForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,13 @@ import { useSelector } from "react-redux";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
-  return <div>TodoList</div>;
+  return (
+    <div>
+      {todos.map(function (todo) {
+        return <div key={todo.id}>{todo.title}</div>;
+      })}
+    </div>
+  );
 };
 
 export default TodoList;
