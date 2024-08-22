450b0ac042e58947ed281c9ea8c07d3b19606777
Jo <johj703@nate.com>
2024-08-22 17:43:47 +0900

    주석처리

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,9 @@
-import { combineReducers, createStore } from "redux";
-import todos from "../modules/todos";
+// import { combineReducers, createStore } from "redux";
+// import todos from "../modules/todos";
 
-const rootReducer = combineReducers({
-  todos: todos,
-});
+// const rootReducer = combineReducers({
+//   todos: todos,
+// });
 
 const store = createStore(rootReducer);
 export default store;
