67419e621104d6f8b2e9569e048bbfe0384e22f8
Jo <johj703@nate.com>
2024-08-22 17:45:37 +0900

    configStore에서 store 작성

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,18 @@
 // import { combineReducers, createStore } from "redux";
 // import todos from "../modules/todos";
 
+import { configureStore } from "@reduxjs/toolkit";
+import todosSlice from "../slices/todosSlice";
+
 // const rootReducer = combineReducers({
 //   todos: todos,
 // });
 
-// const store = createStore(rootReducer);
+//
+
+const store = configureStore({
+  reducer: {
+    todos: todosSlice,
+  },
+});
 export default store;
