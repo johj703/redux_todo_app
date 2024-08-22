750bfcf86a77dd201b0cfa4173d2ee996057e806
Jo <johj703@nate.com>
2024-08-22 17:59:48 +0900

    주석 처리

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,6 +1,6 @@
 import { React, useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../redux/modules/todos";
+// import { addTodo } from "../redux/modules/todos";
 
 const AddForm = () => {
   const [todo, setTodo] = useState("");
