01cb2f026d423e8fedbc308d964e6eee1fc45c44
Jo <johj703@nate.com>
2024-08-22 17:43:23 +0900

    import 구문 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,6 +1,5 @@
-import { combineReducers } from "redux";
+import { combineReducers, createStore } from "redux";
 import todos from "../modules/todos";
-import { createStore } from "redux";
 
 const rootReducer = combineReducers({
   todos: todos,
